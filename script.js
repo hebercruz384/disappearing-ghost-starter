35d07463f64d89e10c2895fac6bb45be37f97b43
Glitch (bitter-artistic-palm) <none>
2020-12-03 23:28:13 +0000

    🍨⛸ Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+$(".hide-button").click(function(){
+  $(".ghost-image").hide();
+});
 
+$(".show-button").click(function(){
+  $(".ghost-image").show();
+});
+
+$(".update-img-button").click(function(){
+  $("img").attr("src","https://cdn.glitch.com/c8fa84e9-fbd9-4cae-8c0a-0fcf25227667%2FDimple.png?v=1605827518167")
+});
+
+$(".message-button").click(function(){
+  let input = $(".input").val();
